bbfe63f976cdf718a0cc274dedf5aba41c9e6c2c
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2019-11-05 18:37:04 +0100

    Update day01

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 int main()
 {
+	int x = 0, sum = 0;
 	std::ifstream in;
-	in.open("data/input.txt");
+	in.open("data/input01.txt");
+	while (!in.eof())
+	{
+		in >> x;
+		sum += x;
+	}
+	in.close();
 
+	std::cout << "Part 1: " << sum << std::endl;
 
+	std::map<int, int> d;
+	bool found = false;
+	sum = 0;
+	
+	in.open("data/input01.txt");
+	while (!found)
+	{
+		while (!in.eof() && !found)
+		{
+			in >> x;
+			sum += x;
+			if (d.count(sum)) found = true;
+			else d.insert(std::pair<int, int>(sum, 1));
+		}
+		in.clear();
+		in.seekg(0, in.beg);
+	}
+	in.close();
+
+	std::cout << "Part 2: " << sum << std::endl;
 }
